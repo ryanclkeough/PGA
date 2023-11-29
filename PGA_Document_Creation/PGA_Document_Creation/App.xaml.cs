37f5f79c90c5b731cb2658dd751b22ded482d327
Ryan <ryankeough0311@users.noreply.github.com>
2023-11-28 22:53:41 -0600

    Updating .gitignore and creating XAML template

---
+++ PGA_Document_Creation/PGA_Document_Creation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PGA_Document_Creation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
