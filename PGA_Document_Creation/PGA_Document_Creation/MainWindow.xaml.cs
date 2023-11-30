c1d02df965802fd0c9d0131d6292462b2337bcfc
Ryan <ryankeough0311@users.noreply.github.com>
2023-11-30 00:07:57 -0600

    Getting date to populate in gui

--- PGA_Document_Creation/PGA_Document_Creation/MainWindow.xaml.cs
+++ PGA_Document_Creation/PGA_Document_Creation/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Runtime.InteropServices.JavaScript;
 using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -40,13 +41,34 @@ namespace PGA_Document_Creation
           }
           else
           {
-            currentDirectory = System.IO.Path.GetDirectoryName(currentDirectory);
-            jsonPath = System.IO.Path.Combine(currentDirectory, "Previous_Information.json");
+            if (currentDirectory != System.IO.Path.GetPathRoot(currentDirectory))
+            {
+              currentDirectory = System.IO.Path.GetDirectoryName(currentDirectory);
+              jsonPath = System.IO.Path.Combine(currentDirectory, "Previous_Information.json");
+            }
+            else
+            {
+              MessageBox.Show($"Error with path: {currentDirectory} is a root directory, and .json was not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
           }
         }
 
         string jsonContent = File.ReadAllText(jsonPath);
-        //JSObject jsonData = JSObject.Parse(jsonContent);
+        JsonDocument jsonDocument = JsonDocument.Parse(jsonContent);
+        JsonElement root = jsonDocument.RootElement;
+        if (root.TryGetProperty("date", out JsonElement displayTextElement))
+        {
+          string displayText = displayTextElement.GetString() ?? "ERROR: JSON DID NOT CONTAIN DATA";
+          Date_TextBox.Text = displayText;
+        }
+        else
+        {
+          MessageBox.Show("Key 'displayText' not found in the JSON file.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        //Date_TextBox.Text = jsonContent;
+
+        //MessageBox.Show($"Hi");
       }
       catch (Exception ex)
       {
